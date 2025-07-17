a4876ff2f7eeaf0a4598da4f072829ad76924198
Dennis <43215753+Dennis-NL@users.noreply.github.com>
2025-07-18 01:59:10 +0200

    Signal

--- board/safety/safety_volkswagen_mlb.h
+++ board/safety/safety_volkswagen_mlb.h
@@ -21,7 +21,7 @@ RxCheck volkswagen_mlb_rx_checks[] = {
   {.msg = {{MSG_ESP_05, 0, 8, .check_checksum = false, .max_counter = 15U, .frequency = 50U}, { 0 }, { 0 }}},
   {.msg = {{MSG_TSK_02, 0, 8, .check_checksum = false, .max_counter = 15U, .frequency = 33U}, { 0 }, { 0 }}},
   {.msg = {{MSG_MOTOR_03, 0, 8, .check_checksum = false, .max_counter = 15U, .frequency = 100U}, { 0 }, { 0 }}},
-  {.msg = {{MSG_ACC_02, 2, 8, .check_checksum = false, .max_counter = 15U, .frequency = 17U}, {0}, {0}}},
+  {.msg = {{MSG_ACC_02, 0, 8, .check_checksum = false, .max_counter = 15U, .frequency = 17U}, {0}, {0}}},
 };
 
 
@@ -136,7 +136,7 @@ static int volkswagen_mlb_fwd_hook(int bus_num, int addr) {
       bus_fwd = 2;
       break;
     case 2:
-      if ((addr == MSG_HCA_01) || (addr == MSG_LDW_02) || (addr == MSG_ACC_02)) {
+      if ((addr == MSG_HCA_01) || (addr == MSG_LDW_02)) {
         // openpilot takes over LKAS steering control and related HUD messages from the camera
         bus_fwd = -1;
       } else {
